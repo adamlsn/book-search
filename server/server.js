54a82f161a170c33bfb8b49c4f1fb2017f701ae6
Adam Olson <adamlsn@gmail.com>
2021-07-26 19:54:44 -0500

    adding in resolvers.js functionality, fiddling with graphql issues

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
@@ -20,14 +19,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build.index.html"))
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
 
 db.once('open', () => {
